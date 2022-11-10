dc452f4de31f72740d78a7b3b562a4cc4366e9c3
alexiajg22 <a20192270@pucp.edu.pe>
2022-11-09 20:30:14 -0500

    creado dao base

---
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Dao/DaoBase.java
@@ -0,0 +1,24 @@
+package com.example.lab9_base.Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DaoBase {
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+
+        String URL = "jdbc:mysql://localhost:3306/";
+        String USER = "root";
+        String PASS = "root";
+        String DB = "hr";
+
+        return DriverManager.getConnection(URL + DB, USER, PASS);
+    }
+}
+
