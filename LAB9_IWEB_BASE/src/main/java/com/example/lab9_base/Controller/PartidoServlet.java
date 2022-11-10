ad43eee85971a12751b3c7e7ec3b07305d8ce516
alexiajg22 <a20192270@pucp.edu.pe>
2022-11-09 22:02:15 -0500

    listar

--- LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Controller/PartidoServlet.java
+++ LAB9_IWEB_BASE/src/main/java/com/example/lab9_base/Controller/PartidoServlet.java
@@ -1,5 +1,6 @@
 package com.example.lab9_base.Controller;
 
+import com.example.lab9_base.Dao.DaoPartidos;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -29,12 +30,11 @@ public class PartidoServlet extends HttpServlet {
 
         String action = request.getParameter("action") == null ? "lista" : request.getParameter("action");
         RequestDispatcher view;
+        DaoPartidos daoPartidos = new DaoPartidos();
         switch (action) {
             case "lista":
-                /*
-                Inserte su código aquí
-                 */
-                view = request.getRequestDispatcher("index.jsp");
+                request.setAttribute("listaPartidos",daoPartidos.listaDePartidos());
+                view = request.getRequestDispatcher("partidos/list.jsp");
                 view.forward(request, response);
                 break;
             case "crear":
